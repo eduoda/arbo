269b76215f51878f45f8017e6e56d6780e158b5c
Oda <oda@member.fsf.org>
2019-07-07 11:25:09 -0300

    I'm a fucking genius

---
+++ test.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+let request = require('supertest');
+request = request('http://localhost:3000');
+
+let {Var} = require('./modules/var');
+let {Permission,Role,RolePermission,PermissionCache} = require('./modules/permission');
+let {User,Token} = require('./modules/user');
+let {Section,Membership,MembershipRole} = require('./modules/section');
+let {Content,ContentSection,Page} = require('./modules/content');
+
+let modules = [
+  Var,
+  User,
+  Section,
+  Membership,
+  Permission,
+  Role,
+  RolePermission,
+  PermissionCache,
+  Token,
+  MembershipRole,
+  Content,
+  ContentSection,
+  Page
+]
+request.data={};
+modules.forEach(mod => {
+  if(mod.test)
+    mod.test(request);
+});
