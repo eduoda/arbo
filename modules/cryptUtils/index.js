9e7fba158944efa9d79c278794809d4a74bdaa9b
Anderson <andersonpiata@hotmail.com>
2019-09-20 17:58:09 -0300

    Replacement of problematic characters.

--- modules/cryptUtils/index.js
+++ modules/cryptUtils/index.js
@@ -2,10 +2,10 @@ const crypto = require('crypto');
 
 // Helper functions
 function hash(salt,msg,enc='base64'){
-  return crypto.createHash('sha256',salt).update(msg).digest(enc);
+  return crypto.createHash('sha256',salt).update(msg).digest(enc).replace("+",".").replace("/","_").replace("=","-");
 }
 function randomString(len){
-  return crypto.randomBytes(1+len).toString('base64').slice(0,len);
+  return crypto.randomBytes(1+len).toString('base64').slice(0,len).replace("+",".").replace("/","_").replace("=","-");
 }
 
 module.exports = {hash,randomString}
