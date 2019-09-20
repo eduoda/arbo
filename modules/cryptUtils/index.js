9caab3d596f9f51c7b7c9b9a5f8c77b95d1ef1ae
Anderson <andersonpiata@hotmail.com>
2019-09-20 16:40:54 -0300

    Change in randomString.

--- modules/cryptUtils/index.js
+++ modules/cryptUtils/index.js
@@ -5,7 +5,7 @@ function hash(salt,msg,enc='base64'){
   return crypto.createHash('sha256',salt).update(msg).digest(enc);
 }
 function randomString(len){
-  return crypto.randomBytes(1+len*3/4).toString('base64').slice(0,len);
+  return crypto.randomBytes(1+len).toString('base64').slice(0,len);
 }
 
 module.exports = {hash,randomString}
