29ac22dc79167d192ce9d960522f6439a112da22
Anderson <andersonpiata@hotmail.com>
2019-10-14 16:54:08 -0300

    Correction on hash.

--- modules/cryptUtils/index.js
+++ modules/cryptUtils/index.js
@@ -2,10 +2,10 @@ const crypto = require('crypto');
 
 // Helper functions
 function hash(salt,msg,enc='base64'){
-  return crypto.createHmac('sha256',salt).update(msg).digest(enc).replace("+",".").replace("/","_").replace("=","-");
+  return crypto.createHmac('sha256',salt).update(msg).digest(enc).replace(/\+/g,".").replace(/\//g,"_").replace(/=/g,"-");
 }
 function randomString(len){
-  return crypto.randomBytes(1+len).toString('base64').slice(0,len).replace("+",".").replace("/","_").replace("=","-");
+  return crypto.randomBytes(1+len).toString('base64').slice(0,len).replace(/\+/g,".").replace(/\//g,"_").replace(/=/g,"-");
 }
 
 module.exports = {hash,randomString}
